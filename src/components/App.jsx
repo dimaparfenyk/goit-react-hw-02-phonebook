350000c8522e15860741d67129eb264e21937b63
dimaparfenyk <92674961+dimaparfenyk@users.noreply.github.com>
2022-07-10 14:02:34 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import ContactForm from "./ContactForm/ContactForm";
+// import ContactList from "./ContactList/ContactList";
+
+export class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  formSubmitHandler = userData => {
+    return this.setState (prevState => ({
+      contacts: [...prevState.contacts, userData],
+    }))
+  };
+
+  // deleteContact = id => {
+  //   // this.setState(prevState => ({
+  //   //   contacts: prevState.contacts.filter(item => item.id !== id)
+  //   // }));
+  // };
+  
+  render() {
+    // const  contacts  = this.state.contacts;
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler}/>
+       
+        <h2>Contacts</h2>
+       
+      </div>
+    );
+  };
 };
+
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+// // import { nanoid } from 'nanoid'
+
+// const ContactList = ({ contacts, onDeleteContact }) => (
+//                 <ul>
+//             {contacts.map(({ id, name, number }) => 
+//                 <li key={id}>
+//                     <p>{name}: {number}</p>
+//                     <button onClick={() => onDeleteContact(id)}>Удалить</button>
+//                 </li>
+//             )}
+//                 </ul>
+//         );
+
+// export default ContactList; 
